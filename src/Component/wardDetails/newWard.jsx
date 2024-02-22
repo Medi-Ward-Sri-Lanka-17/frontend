91d2d143536829d2758c016eb3a042b90672d6cd
Dilki <dilkihansapani321@gmail.com>
2024-02-22 23:05:52 +0530

    add new ward form validation added (newWard.jsx)

--- src/Component/wardDetails/newWard.jsx
+++ src/Component/wardDetails/newWard.jsx
@@ -13,6 +13,7 @@ import {
 import { Formik, Form, Field } from "formik";
 import "react-toastify/dist/ReactToastify.css";
 import Swal from "sweetalert2";
+import { validationSchema } from "./validation";
 
 const AddNewWardForm = ({ open, handleClose }) => {
   const [successMessage, setSuccessMessage] = useState(null);
@@ -48,7 +49,7 @@ const AddNewWardForm = ({ open, handleClose }) => {
     wardName: "",
     wardNumber: "",
     sisterName: "",
-    numberOfNursesInTheWard: "",
+    numberOfNurses: "",
     morningShift: "",
     eveningShift: "",
     nightShift: "",
@@ -57,92 +58,137 @@ const AddNewWardForm = ({ open, handleClose }) => {
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={onSubmit}
-      //   validate={validate}
+      validationSchema={validationSchema}
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          console.log(values);
+          showSuccessAlert();
+          handleClose();
+        } catch (error) {
+          console.error("Error submitting form:", error.message);
+        } finally {
+          setSubmitting(false);
+        }
+      }}
     >
-      <Form>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Add ward details</DialogTitle>
-          <DialogContent>
-            <label>Ward Name</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="wardName"
-              required
-            />
-            <label>Ward number</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="wardNumber"
-              required
-            />
-            <label>Sister name</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="sisterName"
-              required
-            />
+      {({
+        handleChange,
+        values,
+        isSubmitting,
+        handleSubmit,
+        touched, //touched property in Formik, it is a state variable that keeps track of which fields have been "touched" or interacted with.
+        errors,
+      }) => (
+        <Form>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add ward details</DialogTitle>
+            <DialogContent>
+              <label>Ward Name</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="wardName"
+                required
+                onChange={handleChange}
+                error={touched.wardName && Boolean(errors.wardName)}
+                helperText={touched.wardName && errors.wardName}
+              />
+              <label>Ward number</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="wardNumber"
+                required
+                onChange={handleChange}
+                error={touched.wardNumber && Boolean(errors.wardNumber)}
+                helperText={touched.wardNumber && errors.wardNumber}
+              />
+              <label>Sister name</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="sisterName"
+                required
+                onChange={handleChange}
+                error={touched.sisterName && Boolean(errors.sisterName)}
+                helperText={touched.sisterName && errors.sisterName}
+              />
 
-            <label>Total number of nurses in ward</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="numberOfNursesInTheWard"
-              required
-            />
+              <label>Total number of nurses in ward</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="numberOfNurses"
+                required
+                onChange={handleChange}
+                error={touched.numberOfNurses && Boolean(errors.numberOfNurses)}
+                helperText={touched.numberOfNurses && errors.numberOfNurses}
+              />
 
-            <label>Number of nurses in morning shift</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="morningShift"
-              required
-              select
-            />
+              <label>Number of nurses in morning shift</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="morningShift"
+                required
+                onChange={handleChange}
+                error={touched.morningShift && Boolean(errors.morningShift)}
+                helperText={touched.morningShift && errors.morningShift}
+              />
 
-            <label>Number of nurses in evening shift</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="eveningShift"
-              required
-            />
+              <label>Number of nurses in evening shift</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="eveningShift"
+                required
+                onChange={handleChange}
+                error={touched.eveningShift && Boolean(errors.eveningShift)}
+                helperText={touched.eveningShift && errors.eveningShift}
+              />
 
-            <label>Number of nurses in night shift</label>
-            <Field
-              as={TextField}
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="nightShift"
-              required
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button color="primary" onClick={showSuccessAlert}>
-              Save
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Form>
+              <label>Number of nurses in night shift</label>
+              <Field
+                as={TextField}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                name="nightShift"
+                required
+                onChange={handleChange}
+                error={touched.nightShift && Boolean(errors.nightShift)}
+                helperText={touched.nightShift && errors.nightShift}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                disabled={isSubmitting}
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
+                Save
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Form>
+      )}
     </Formik>
   );
 };
