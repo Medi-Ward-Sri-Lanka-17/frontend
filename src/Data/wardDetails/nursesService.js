45ec9db76a78673f18eada53611944e1b9d65b53
Dilki <dilkihansapani321@gmail.com>
2024-02-24 20:47:22 +0530

    ward staff member table appearence updated

--- src/Data/wardDetails/nursesService.js
+++ src/Data/wardDetails/nursesService.js
@@ -5,19 +5,37 @@
 //const API_URL = 'your_api_url_here';
 
 const rows = [
-  { id: 1, name: 'John Doe', serviceId: 'S123' },
-  { id: 2, name: 'Jane Smith', serviceId: 'S456' },
-  { id: 3, name: 'Bob Johnson', serviceId: 'S789' },
-]
+  {
+    id: 1,
+    serviceId: "199965500338",
+    name: "John Doe",
+    mobileNo: "0775521604",
+    email: "dilki99hansapani@gmail.com",
+  },
+  {
+    id: 2,
+    serviceId: "199965852845",
+    name: "Jane Smith",
+    mobileNo: "0766640472",
+    email: "dilkihansapani789@gmail.com",
+  },
+  {
+    id: 3,
+    serviceId: "199965880047",
+    name: "Bob Johnson",
+    mobileNo: "0774100672",
+    email: "bob99john@gmail.com",
+  },
+];
 
 export const getNurses = async () => {
   try {
     //const response = await axios.get(`${API_URL}/nurses`);
 
     //return response.data;
-    return rows
+    return rows;
   } catch (error) {
-    console.error('Error fetching nurses:', error)
-    throw error
+    console.error("Error fetching nurses:", error);
+    throw error;
   }
-}
+};
