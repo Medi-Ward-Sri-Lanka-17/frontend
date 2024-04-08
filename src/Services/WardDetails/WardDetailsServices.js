8ca90f61c2d14af63ab887935041f4557b6925f4
Dilki <dilkihansapani321@gmail.com>
2024-04-08 20:25:43 +0530

    add new ward connected with the backend

--- src/Services/WardDetails/WardDetailsServices.js
+++ src/Services/WardDetails/WardDetailsServices.js
@@ -1,11 +1,12 @@
-import { apiClient } from '../../Api/ApiClient'
+import { apiClient } from "../../Api/ApiClient";
 
 export const addWard = async (value) => {
   try {
-    const response = await apiClient.post('/ward/add', value)
-    return response.status
+    const response = await apiClient.post("/add-ward", value);
+    console.log(response.status);
+    return response.status;
   } catch (err) {
-    throw new Error("Couldn't add")
-    console.error(err)
+    throw new Error("Couldn't add");
+    console.error(err);
   }
-}
+};
