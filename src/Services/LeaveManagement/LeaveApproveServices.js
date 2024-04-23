b6ec005398dc9829242e7df24232c01f6fb8bfaf
dilina683 <weerasinghe.dilina683@gmail.com>
2024-04-23 15:15:05 +0530

    update leave approve section

--- src/Services/LeaveManagement/LeaveApproveServices.js
+++ src/Services/LeaveManagement/LeaveApproveServices.js
@@ -1,11 +1,15 @@
 import { apiClient } from '../../Api/ApiClient'
 import { showSuccessAlert, showUnsuccessAlert } from '../../Component/ShowAlert'
 
-export const getWardList = async (nic) => {
-  const wards = []
+export const getWardList = async (nic, position) => {
+  var wards = []
   try {
-    const response = await apiClient.get(`/show-ward/matron/${nic}`)
-    return response.data
+    if (position == 'Matron') {
+      const response = await apiClient.get(`/show-ward/matron/${nic}`)
+      wards = response.data
+    }
+
+    return wards
   } catch (error) {
     if (error.response) {
       console.error('Error Response:', error.response.data)
@@ -76,6 +80,67 @@ export const approveLeaveByMatron = async (leaveID) => {
   try {
     const response = await apiClient.put(`/leaveApprove/matron`, leaveID)
     showSuccessAlert(response.data)
+  } catch (error) {
+    if (error.response) {
+      console.error('Error Response:', error.response.data)
+      showUnsuccessAlert(error.response.data)
+    } else if (error.request) {
+      console.error('Error Request:', error.request)
+      showUnsuccessAlert('No response from server')
+    } else {
+      console.error('Error Message:', error.message)
+      showUnsuccessAlert('Error: ' + error.message)
+    }
+  }
+}
+
+//Decline leave request
+
+export const declineLeaveRequest = async (leaveID) => {
+  try {
+    const response = await apiClient.put(`/leaveApprove/decline`, leaveID)
+    showSuccessAlert(response.data)
+  } catch (error) {
+    if (error.response) {
+      console.error('Error Response:', error.response.data)
+      showUnsuccessAlert(error.response.data)
+    } else if (error.request) {
+      console.error('Error Request:', error.request)
+      showUnsuccessAlert('No response from server')
+    } else {
+      console.error('Error Message:', error.message)
+      showUnsuccessAlert('Error: ' + error.message)
+    }
+  }
+}
+
+//to get more details about leave
+
+export const getMoreLeaveDetails = async (leaveId) => {
+  try {
+    const response = await apiClient.get(`/leaveApprove/more/${leaveId}`)
+    return response.data
+  } catch (error) {
+    if (error.response) {
+      console.error('Error Response:', error.response.data)
+      showUnsuccessAlert(error.response.data)
+    } else if (error.request) {
+      console.error('Error Request:', error.request)
+      showUnsuccessAlert('No response from server')
+    } else {
+      console.error('Error Message:', error.message)
+      showUnsuccessAlert('Error: ' + error.message)
+    }
+  }
+}
+
+export const getPreiousLeaveDetails = async (leaveId) => {
+  try {
+    const response = await apiClient.get(
+      `leaveApprove/more/previousRecode/${leaveId}`
+    )
+    console.log(response)
+    return response.data
   } catch (error) {
     if (error.response) {
       console.error('Error Response:', error.response.data)
